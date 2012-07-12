45ec268a2b4b9cc44da01ccb95f829b0a02abfbb
Patrick <Patrick_DJ@163.com>
2012-07-12 17:06:34 +0800

    .

---
+++ deps/libc/include/sl/sl_libc_defs.h
@@ -0,0 +1,11 @@
+#ifndef __SL_DEFS_H__
+#define __SL_DEFS_H__
+
+#define     int         sl_i;   /* signed int. */
+#define     char        sl_c;   /* only use for data. for string, please use sl_string_t instead. */
+#define     int         sl_b;   /* boolean */
+#define     float       sl_f;   /* DO NOT use double type. */
+
+#define     extern      SL_API
+
+#endif
