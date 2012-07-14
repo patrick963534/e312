87973bbccdac15975f776f18fc4209a74f9b982c
Patrick <tangramgis@163.com>
2012-07-14 08:20:44 +0800

    .

--- deps/libsl/src/sl_string.c
+++ deps/libsl/src/sl_string.c
@@ -1,4 +1,5 @@
 #include <sl/sl_string.h>
+#include <sl/sl_memory.h>
 
 SL_API sl_string_t* sl_string_new()
 {
@@ -7,15 +8,14 @@ SL_API sl_string_t* sl_string_new()
     me = (sl_string_t*)sl_object_new(sizeof(*me));
     me->destruct = (sl_destruct_func)sl_string_destruct;
     me->sz = 32;
-    me->buf = sl_memory_new(me->sz);
-    me->is_const = 0;
+    me->buf = (sl_c*)sl_memory_new(me->sz);
 
     return me;
 }
 
 SL_API sl_string_t* sl_string_from_char_array(const sl_c *str)
 {
-
+    return 0;
 }
 
 SL_API void sl_string_destruct(sl_string_t *me)
