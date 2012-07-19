1fc9fad24fc4b04dc05ef54dd640e45de079c413
Patrick <Patrick_DJ@163.com>
2012-07-19 14:18:17 +0800

    .

--- deps/libsl/include/sl/sl_memory.h
+++ deps/libsl/include/sl/sl_memory.h
@@ -2,31 +2,17 @@
 #define __SL_MEMORY_H__
 
 #include <sl/sl_defs.h>
-#include <sl/sl_mld.h>
 
-/**
- Similar with bzero, lower performance one, but it's OK for me. 
-*/
-SL_API void     sl_memory_zero(void *p, sl_i sz);
+#define SL_DEBUG_FILE_INFO_ARGS   __FILE__, __FUNCTION__, __LINE__
+#define SL_DEBUG_FILE_INFO_PARAM  const sl_c *file, const sl_c *func, sl_i line
 
-/**
- Which is the very base malloc memory method.
-*/
-SL_API void*    sl_memory_new_(sl_i sz);
+SL_API void   sl_memory_zero(void *p, sl_i sz);
+SL_API void*  sl_memory_new_(sl_i sz, SL_DEBUG_FILE_INFO_PARAM);
+SL_API void*  sl_memory_renew_(void *p, sl_i sz, SL_DEBUG_FILE_INFO_PARAM);
+SL_API void   sl_memory_delete_(void *p, SL_DEBUG_FILE_INFO_PARAM);
 
-/**
- Which is the very base realloc memory method.
-*/
-SL_API void*    sl_memory_renew_(void *p, sl_i sz);
-
-/**
- Which is the very base free memory method.
-*/
-SL_API void*    sl_memory_delete_(void *p);
-
-#define SL_DEBUG_FILE_INFO  __FILE__, __FUNCTION__, __LINE__
-#define sl_memory_new(sz)      sl_mld_new(sz, SL_DEBUG_FILE_INFO)
-#define sl_memory_renew(p, sz) sl_mld_renew(p, sz, SL_DEBUG_FILE_INFO)
-#define sl_memory_delete(p)    sl_mld_delete(p, SL_DEBUG_FILE_INFO)
+#define sl_memory_new(sz)      sl_memory_new_(sz, SL_DEBUG_FILE_INFO)
+#define sl_memory_renew(p, sz) sl_memory_renew_(p, sz, SL_DEBUG_FILE_INFO)
+#define sl_memory_delete(p)    sl_memory_delete_(p, SL_DEBUG_FILE_INFO)
 
 #endif
