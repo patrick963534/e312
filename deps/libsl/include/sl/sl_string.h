e5acf3d8595fd413b1259e39fd49ceba55bad5c2
patrick <patrick_dj@163.com>
2012-07-27 17:49:56 +0800

    .

--- deps/libsl/include/sl/sl_string.h
+++ deps/libsl/include/sl/sl_string.h
@@ -20,8 +20,9 @@ typedef struct sl_char_t
 SL_API const sl_char_t sl_char_from(const sl_c *s);
 
 /**
- Both sl_c* and sl_string_t are used to identify a UTF-8.
- Only sl_string_t can used to do modification.
+ Description:
+     Both sl_c* and sl_string_t are used to identify a UTF-8.
+     Only sl_string_t can used to do modification.
 */ 
 typedef struct sl_string_t
 {
@@ -33,13 +34,15 @@ typedef struct sl_string_t
 } sl_string_t;
 
 /**
- Create a sl_string_t by an UTF-8 string.
+ Description:
+     Create a sl_string_t by an UTF-8 string.
  @str: Assert @str is an UTF-8 string.
 */
 SL_API sl_string_t* sl_string_new(const sl_c *str);
 
 /**
- Format a string to create a sl_string_t instance.
+ Description:
+     Format a string to create a sl_string_t instance.
  @fmt: Assert @fmt is an UTF-8 string.
  @sz:  The maximize size of buffer. 
  @return: If the real result larger than sz, then return NULL.
@@ -48,15 +51,17 @@ SL_API sl_string_t* sl_string_new(const sl_c *str);
 SL_API sl_string_t* sl_string_format(sl_i sz, const sl_c *fmt, ...);
 
 /**
- Append a @str string to the end of @me string.
+ Description:
+     Append a @str string to the end of @me string.
  @me: Assert @me.c_str is an UTF-8 string.
  @str: Assert it's an UTF-8 string.
 */
 SL_API void         sl_string_append(sl_string_t *me, const sl_c *str);
 
 /**
- Get a small string to indicate a UTF-8 multi-bytes character.
- Get the @n mutli-byte character in @me string.
+ Description:
+     Get a small string to indicate a UTF-8 multi-bytes character.
+     Get the @n mutli-byte character in @me string.
  @n: The nth multi-byte character.
  @return: It's a UTF-8 string, with a multi-byte character.
 */
